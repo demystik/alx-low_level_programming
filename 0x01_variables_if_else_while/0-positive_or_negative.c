dffcc39d83e2a878802c4a2c8e46b022bc40db89
demystik <demystiktech@gmail.com>
2022-06-03 08:44:37 -0700

    task 0

---
+++ 0x01_variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ * Description: A program that pints the alphabet in lowercase
+ * Return: Always
+ */
+
+
+
+int main(void)
+
+{
+
+int c;
+
+for(c = 97; c <= 122; c++)
+
+putchar(c);
+
+return (0);
+
+}
