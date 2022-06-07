4237e5d5ae2d846988799173307abc36c4d5283d
demystik <demystiktech@gmail.com>
2022-06-07 13:46:33 -0700

    task6

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,12 +10,12 @@
 
 int print_sign(int n)
 {
-if(n > 0)
+if (n > 0)
 {
 _putchar('+');
 return (1);
 }
-else if(n == 0)
+else if (n == 0)
 {
 _putchar('0');
 return (0);
@@ -23,7 +23,7 @@ return (0);
 else
 {
 _putchar('-');
-return(-1);
+return (-1);
 }
 
 }
