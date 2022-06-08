d417d482c042a75e86e0a7b723d108adf84f2a03
demystik <demystiktech@gmail.com>
2022-06-07 19:44:25 -0700

    task12

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,14 +10,14 @@
 
 void print_to_98(int n)
 {
-if(n <= 98)
+if (n <= 98)
 {
-for(n = n; n <= 98; n++)
+for (n = n; n <= 98; n++)
 printf("%i, ", n);
 }
 else
 {
-for(n =n; n >= 98; n--)
+for (n =n; n >= 98; n--)
 printf("%i, ", n);
 }
 
