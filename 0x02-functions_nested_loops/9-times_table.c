6c098a83ba790538c4a9bb8faad02a444ae2c8a1
demystik <demystiktech@gmail.com>
2022-06-07 19:10:31 -0700

    task10

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,13 +13,13 @@ void times_table(void)
 int num, nums;
 
 
-for(num = 0; num <= 9; num++)
+for (num = 0; num <= 9; num++)
 {
 for (nums = 0; nums <= 9; nums++)
 {
 printf("%2d", num * nums);
 if(nums != 9)
-   putchar(',');
+putchar(',');
 
 putchar(' ');
 }
