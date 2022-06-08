3c7dd3c6ef1d9ebb1341290f1b5acf9c17665daf
demystik <demystiktech@gmail.com>
2022-06-07 19:05:57 -0700

    task6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,7 +3,7 @@
 
 /**
  * _abs - This program prints absolute value of an integer
- *
+ * @num: the input
  * Description: To print an absolute value
  * Return: return an int
  */
@@ -11,10 +11,10 @@
 int _abs(int num)
 {
 
-if(num > 0)
-return(num);
-else if(num == 0)
-return(0);
+if (num > 0)
+return (num);
+else if (num == 0)
+return (0);
 else
-return(-1 * num);
+return (-1 * num);
 }
