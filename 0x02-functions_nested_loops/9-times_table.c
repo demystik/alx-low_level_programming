c11f58c1d968fd5a5b0fed4abb568dce1b17efec
demystik <demystiktech@gmail.com>
2022-06-07 19:25:37 -0700

    task10

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,11 +17,10 @@ for (num = 0; num <= 9; num++)
 {
 for (nums = 0; nums <= 9; nums++)
 {
-printf("%d", num * nums);
+printf("%2d", num * nums);
 if (nums != 9)
 putchar(',');
 
-putchar(' ');
 putchar(' ');
 }
 putchar('\n');
