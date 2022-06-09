f4872aac1024fcb3c3f1a904278c831b1a2afbe9
demystik <demystiktech@gmail.com>
2022-06-08 23:21:41 -0700

    push

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,13 +13,17 @@ void print_times_table(int n)
 {
 int num, nums;
 
-if(n <= 15)
+if(!(n > 15) && !(n < 0))
 {
 for(num = 0; num <= n; num++)
 {
 for(nums = 0; nums <= n; nums++)
 {
-printf("%3d, ", num * nums);
+printf("%3d", num * nums);
+if (nums != n)
+{
+printf(", ");
+}
 }
 printf("\n");
 }
