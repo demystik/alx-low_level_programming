98bcf7eb4518bbc84b3cf5c046df4d7028a5dc69
demystik <demystiktech@gmail.com>
2022-06-10 05:02:06 -0700

    task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,28 +9,29 @@
 
 int main(void)
 {
-int n, m;
+int n;
 
 for (n = 1; n <= 100; n++)
 {
 if (n % 3 == 0 && n % 5 == 0)
 {
-printf("FizzBuzz ");
+printf("FizzBuzz");
 }
 else if (n % 3 == 0)
 {
-printf("Fizz ");
+printf("Fizz");
 }
 else if (n % 5 == 0)
 {
-printf("Buzz ");
+printf("Buzz");
 }
 else
 {
 printf("%i ",n);
 }
+if (n != 100)
+printf(" ");
 }
 printf("\n");
-
 return 0;
 }
