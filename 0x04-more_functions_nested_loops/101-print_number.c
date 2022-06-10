6f2a92528099df803d77fad40be5b3e413f22c92
demystik <demystiktech@gmail.com>
2022-06-10 05:19:40 -0700

    advance

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * print_number - This method prints number
+ * @n: the input
+ *
+ * Return: nothing
+ */
+
+void print_number(int n)
+{
+
+unsigned int num = n;
+
+if (n < 0)
+{
+_putchar('-');
+num = -num;
+}
+
+if ((num / 10) > 0)
+print_number(num / 10);
+
+_putchar((num % 10) + '0');
+
+}
