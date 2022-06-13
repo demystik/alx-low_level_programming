b8f869eb2f221deb000e12423451935b253230f6
demystik <demystiktech@gmail.com>
2022-06-13 21:04:30 +0100

    advance stuff

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,7 +13,7 @@ int _atoi(char *s)
 int sign = 1;
 unsigned int num = 0;
 
-do{
+do {
 
 if (*s == '-')
 sign *= -1;
@@ -24,7 +24,7 @@ num = (num * 10) + (*s - '0');
 else if (num > 0)
 break;
 
-}while (*s++);
+} while (*s++);
 
 return (num *sign);
 
