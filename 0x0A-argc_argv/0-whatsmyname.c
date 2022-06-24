15483fe4c0fa9e4c3a295a4c624e2500a2fccfe7
demystik <demystiktech@gmail.com>
2022-06-24 16:00:50 +0100

    task 0 of argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,15 @@
-#include <stdio.h>
+#include<stdio.h>
 #define UNUSED(x) (void)(x)
 /**
- * main - This program prints its name
- *
- * @argv: argument vector
- * @argc: argument count
- * Return: nothing to return
- */
-
-int main(int argc, char *argv[])
+* main - function
+*
+* @argv: the chaine of chaine
+* @argc: the int
+* Return: Always 0.
+*/
+int main(int argc, char **argv)
 {
 UNUSED(argc);
-printf("%s", argv[0]);
+printf("%s\n", argv[0]);
 return (0);
 }
