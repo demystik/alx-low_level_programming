1e4b93f5cce8ac51ee2a0a7cb68d0e1702035ff6
demystik <demystiktech@gmail.com>
2022-07-01 11:21:51 +0100

    realloc task ammended

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -6,20 +6,19 @@
  * @p: pointer to the old memory
  * @np: pointer to the new memory
  * @old: size of old memory
- * @new: size of the new memory
  * Return: return pointer to the new allocate memory
  */
 
 int *_demystik(int *p, int *np, unsigned int old)
 {
-        unsigned int i;
+	unsigned int i;
 
-        for (i = 0; i < old; i++)
-        {
-                np[i] = p[i];
-        }
+	for (i = 0; i < old; i++)
+	{
+		np[i] = p[i];
+	}
 
-        return (np);
+return (np);
 }
 
 
@@ -51,17 +50,17 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		}
 
 	}
-	
+
 
 	if (new_size == 0 && ptr != NULL)
 	{
-		free (ptr);
+		free(ptr);
 		return (NULL);
 	}
 
 		total = sizeof(int) * (old_size + new_size);
 		n_ptr = malloc(total);
-                _demystik(ptr, n_ptr, old_size);
-                return (n_ptr);
+		_demystik(ptr, n_ptr, old_size);
+		return (n_ptr);
 return (n_ptr);
 }
