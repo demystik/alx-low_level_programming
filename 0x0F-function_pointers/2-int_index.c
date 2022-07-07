cb04936c5ff3c56080eae2a53ca91c9ec02154db
demystik <demystiktech@gmail.com>
2022-07-07 18:06:36 +0100

    task 2-int_index adjusted

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,19 +6,24 @@
  * @array: The name of the array in which the elements are
  * @size: The number of the elements in the array array
  * @cmp: The pointer to a function to be used to compare values
+ * Return: index of the first occurence of the element provided
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, check;
 
-	if (size <= 0)
-		return (-1);
-
-	for (i = 0; i < size; i++)
+	if (array && cmp)
 	{
-		check = cmp(array[i]);
-		if (check == 1)
-			return (i);
+		if (size <= 0)
+			return (-1);
+
+		for (i = 0; i < size; i++)
+		{
+			check = cmp(array[i]);
+			if (check == 1)
+				return (i);
+		}
+
 	}
 	return (-1);
 }
