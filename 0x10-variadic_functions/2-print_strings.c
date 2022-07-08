d4842841caccd92909a17e9d7c384efe7ef3e5e3
demystik <demystiktech@gmail.com>
2022-07-08 12:59:57 +0100

    task 1 fixed

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,6 +1,14 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 
+/**
+ * print_strings - Prints strings, followed by a new line.
+ * @separator: The string to be printed between strings.
+ * @n: The number of strings passed to the function.
+ * @...: A variable number of strings to be printed.
+ *
+ * Description: If separator is NULL, it is not printed.
+ */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ptr;
