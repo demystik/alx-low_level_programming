30703551c3baedaad68c8c84593131bc313600ec
demystik <demystiktech@gmail.com>
2022-07-08 12:34:16 +0100

    task 1 fixed

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,7 +17,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(ptr, n);
 
 
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 
 		printf("%d", va_arg(ptr, int));
