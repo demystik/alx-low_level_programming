f39ecb328ee91ff383e48b9eacb35a29e365eb44
demystik <demystiktech@gmail.com>
2022-07-08 13:17:59 +0100

    task 2 fixed

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -28,7 +28,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		if (i != (n - 1) && separator != NULL)
 		{
-			printf("%c ", *separator);
+			printf("%s ", separator);
 		}
 	}
 
