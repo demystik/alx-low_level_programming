20dffcca53f5641eb088108dd2c3e4596606e502
demystik <demystiktech@gmail.com>
2022-07-18 21:57:24 +0100

    hot fixing

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,9 +9,7 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t len;
-
-	len = 0;
+	size_t len = 0;
 
 	while (h != NULL)
 	{
@@ -19,5 +17,5 @@ size_t print_listint(const listint_t *h)
 		h = h->next;
 		len++;
 	}
-return (len);
+	return (len);
 }
--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -13,8 +13,8 @@ void free_listint(listint_t *head)
 
 	while ((node = head) != NULL)
 	{
-		free(node);
 		head = head->next;
+		free(node);
 	}
 
 }
