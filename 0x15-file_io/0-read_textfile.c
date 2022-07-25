d0a79b3a9fbf3276b79741d20d17dac587f102a5
demystik <demystiktech@gmail.com>
2022-07-25 17:24:09 +0100

    push all tasks

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
- * read_textfile - This function reads a text file and print
- * it to the posix standart output
- * @filename: The name of the file
- * @letters: The letters to print
- * Return: the actual numbers of letters it could read and print
- * or 0 if the file can not be opened or read
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t num_read, num_word;
-	char *buffer;
+	ssize_t nrd, nwr;
+	char *buf;
 
-	if (!(filename))
+	if (!filename)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
@@ -22,18 +21,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 
-	buffer = malloc(sizeof(char) * (letters));
-	if (!buffer)
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
 		return (0);
 
-	num_read = read(fd, buffer, letters);
-	num_word = write(STDOUT_FILENO, buffer, num_read);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
 	close(fd);
 
-	free(buffer);
+	free(buf);
 
-	return (num_word);
-
+	return (nwr);
 }
-
--- 0x15-file_io/README.md
+++ 0x15-file_io/README.md
@@ -1,1 +1,25 @@
-This is a readme file for file i/o
+-Hi
+
+
+# C - ALX low level programming & Algorithm
+
+In this section of the journey to software engineering world, i have learned so far a lot of things among which are listed below.
+
+## Low level programming :heavy_check_mark:
+
+`How to print Hello, World in C`
+`Variable, if, else, while`
+`Data-types`
+`Functions, nested loops`
+`Debugging`
+`Pointers`
+`Arrays`
+`Strings`
+`Static library`
+`Recursion`
+`argc & argv`
+`Dynamic memory allocation`
+`Structures`
+`Function pointers`
+`Variadic functions`
+`and more to come..`
