59b2ebed8ec4f1e564120fa58943d1721db97644
demystik <thaoban@WIZARD.localdomain>
2022-09-01 17:49:16 +0100

    task 5 done!

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -11,16 +11,14 @@
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	dlistint_t *tmp;
 
-	tmp = head;
 	if (head == NULL)
 		return (NULL);
 
-	while (index != 0 && tmp->next != NULL)
+	while (index != 0 && head->next != NULL)
 	{
-		tmp = tmp->next;
+		head = head->next;
 		index--;
 	}
-return (tmp);
+return (head);
 }
