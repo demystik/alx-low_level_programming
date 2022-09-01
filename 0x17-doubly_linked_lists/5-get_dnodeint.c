a8c537661466797476de263528b61395af2cb458
demystik <thaoban@WIZARD.localdomain>
2022-09-01 18:01:24 +0100

    hot fix

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	while (head->prev != NULL)
 		head = head->prev;
 
-	while (index != 0)
+	while (index != 0 && head != NULL)
 	{
 		head = head->next;
 		index--;
