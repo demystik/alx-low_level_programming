26330710a6228b79ec1c78c5c6f9021d4c089eeb
demystik <demystiktech@gmail.com>
2022-09-05 13:59:05 +0100

    also dynamically commiting

---
+++ 0x18-dynamic_libraries/op/add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * add - This function add two numbers
+ * @x: fist number
+ * @y: second number
+ * Author - Thaoban Abdrasheed
+ *
+ * Return: return addition of two numbers
+ */
+int add(int x, int y)
+{
+return (x + y);
+}
---
+++ 0x18-dynamic_libraries/op/div.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * div - This function divide two numbers
+ * @x: fist number
+ * @y: second number
+ * Author - Thaoban Abdrasheed
+ *
+ * Return: return division of two numbers
+ */
+int div(int x, int y)
+{
+	return (x/y);
+}
