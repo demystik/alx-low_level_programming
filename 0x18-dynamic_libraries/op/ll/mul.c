79e1054aefbdf63cb084646d99a5ab33c28b3812
demystik <demystiktech@gmail.com>
2022-09-05 18:09:42 +0100

    pupu!

---
+++ 0x18-dynamic_libraries/op/ll/mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * mul - This function multiply two numbers
+ * @x: fist number
+ * @y: second number
+ * Author - Thaoban Abdrasheed
+ *
+ * Return: return multiplications of two numbers
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
---
+++ 0x18-dynamic_libraries/op/ll/sub.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * sub - This function substract two numbers
+ * @x: fist number
+ * @y: second number
+ * Author - Thaoban Abdrasheed
+ *
+ * Return: return substraction of two numbers
+ */
+
+int sub(int x, int y)
+{
+return (x - y);
+}
