5e39d6417b2f2373d0bf23ab529539d3326c0682
demystik <demystiktech@gmail.com>
2022-09-25 16:19:31 +0100

    task 2 fixed

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -11,9 +11,9 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int hash_value;
+unsigned long int hash_value;
 
-    hash_value = hash_djb2(key);
+hash_value = hash_djb2(key);
 
-    return(hash_value % size);
+return (hash_value % size);
 }
