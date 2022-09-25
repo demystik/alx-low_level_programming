6ba774cd776cb543833af427e495c8afbed5e168
demystik <demystiktech@gmail.com>
2022-09-25 15:56:08 +0100

    task 3 added

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,38 +1,51 @@
 #include "hash_tables.h"
-
 /**
- * hash_table_set - This function adds an
- * element to the hash table
+ * hash_table_set - This function adds an * element to thehash table
  * @ht: is the hash table you want to add
- * or update the key/value to
  * @key: is the key. key can not be an empty string
- * @value: value associated with the key. value must 
- * be duplicated. value can be an empty string
- *
+ * @value: value associated with the key
  * Return: 1 if it succeeded, 0 otherwise
  */
-
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    hash_node_t *new_node;
-    hash_table_t *ptr;
-    unsigned long int hashed_value;
-
-    if (!ht)
-        return (NULL)
-    ptr = ht;
-
-    ptr->array malloc(sizeof(hash_node_t));
-    hashed_value = key_index((unsigned char *)key, 1024);
-    ptr->array->key = key;
-    ptr->array->value = ;
-
-    
-
-
-
-
-
-
-
+hash_node_t *new_node;
+hash_table_t *ptr;
+char *copied_value;
+unsigned long int hashed_value, i;
+ptr = ht;
+if (ptr == NULL || key == NULL || value == NULL)
+{
+return (0);
+}
+copied_value = strdup(value);
+if (copied_value == NULL)
+{
+return (0);
+}
+hashed_value = key_index((const unsigned char *)key, ptr->size);
+for (i = hashed_value; ptr->array[i]; i++)
+{
+if (strcmp(ptr->array[i]->key, key) == 0)
+{
+free(ptr->array[i]->value);
+ptr->array[i]->value = copied_value;
+return (1);
+}
+}
+new_node = malloc(sizeof(hash_node_t));
+if (new_node == NULL)
+{
+free(copied_value);
+return (0);
+}
+new_node->key = strdup(key);
+if (new_node->key == NULL)
+{
+free(new_node);
+return (0);
+}
+new_node->value = copied_value;
+new_node->next = ptr->array[hashed_value];
+ptr->array[hashed_value] = new_node;
+return (1);
 }
