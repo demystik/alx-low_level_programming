97da16f4f2669783da8719b314290bbff346bd74
demystik <demystiktech@gmail.com>
2023-04-12 16:21:49 +0100

    task 1 adjusted

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,16 +13,14 @@ int linear_search(int *array, size_t size, int value)
 {
 size_t index;
 
-index = 0;
-
 if (array == NULL)
 	return (-1);
-while (index < size)
+
+for (index = 0; index < size; index++)
 {
 	printf("value checked array[%ld] = [%d]\n", index, array[index]);
 	if (array[index] == value)
 		return (index);
-	index++;
 }
 return (-1);
 }
