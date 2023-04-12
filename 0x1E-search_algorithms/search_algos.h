a5b64df3d6dcc85bf47a8bdd7636d724d7f31e49
demystik <demystiktech@gmail.com>
2023-04-12 16:47:48 +0100

    search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,7 +2,7 @@
 #define SEARCH_ALGOS_H_
 
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
 
 int j_search(int *ptr, size_t size, int value, size_t l, size_t r, size_t step);
 int linear_search(int *array, size_t size, int value);
