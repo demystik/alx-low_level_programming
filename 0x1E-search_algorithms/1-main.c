711de8c32beee52bc63ccf3eef14f33c77953a55
demystik <demystiktech@gmail.com>
2023-07-15 17:32:06 +0100

    Update

--- 0x1E-search_algorithms/1-main.c
+++ 0x1E-search_algorithms/1-main.c
@@ -1,56 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "sort.h"
-
-/**
- * create_listint - Creates a doubly linked list from an array of integers
- *
- * @array: Array to convert to a doubly linked list
- * @size: Size of the array
- *
- * Return: Pointer to the first element of the created list. NULL on failure
- */
-listint_t *create_listint(const int *array, size_t size)
-{
-    listint_t *list;
-    listint_t *node;
-    int *tmp;
-
-    list = NULL;
-    while (size--)
-    {
-        node = malloc(sizeof(*node));
-        if (!node)
-            return (NULL);
-        tmp = (int *)&node->n;
-        *tmp = array[size];
-        node->next = list;
-        node->prev = NULL;
-        list = node;
-        if (list->next)
-            list->next->prev = list;
-    }
-    return (list);
-}
+#include "search_algos.h"
 
 /**
  * main - Entry point
  *
- * Return: Always 0
+ * Return: Always EXIT_SUCCESS
  */
 int main(void)
 {
-    listint_t *list;
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-    size_t n = sizeof(array) / sizeof(array[0]);
+    int array[] = {
+	    0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+    };
+    size_t size = sizeof(array) / sizeof(array[0]);
 
-    list = create_listint(array, n);
-    if (!list)
-        return (1);
-    print_list(list);
-    printf("\n");
-    insertion_sort_list(&list);
-    printf("\n");
-    print_list(list);
-    return (0);
+    printf("Found %d at index: %d\n\n", 2, binary_search(array, size, 2));
+    printf("Found %d at index: %d\n\n", 5, binary_search(array, 5, 5));
+    printf("Found %d at index: %d\n", 999, binary_search(array, size, 999));
+    return (EXIT_SUCCESS);
 }
