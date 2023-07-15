776bbe00fb75752463872599d9b191eefbce1e91
demystik <demystiktech@gmail.com>
2023-07-15 17:33:03 +0100

    Update

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,15 @@
+#ifndef SEARCH_ALGOS_H_
+#define SEARCH_ALGOS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int advanced_binary(int *array, size_t size, int value);
+int j_search(int *ptr, size_t size, int value,
+	      size_t l, size_t r, size_t step);
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int b_search(int *ptr, int value, size_t l, size_t r);
+
+#endif /* SEARCH_ALGOS_H */
